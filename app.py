bc3b75f237b35ca19e6d786e501097ed2e442d50
gabriel silva <gabrielturing@gmail.com>
2022-02-10 01:20:06 -0300

    putil get pid tmsrv

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
 import ctypes
-from ReadWriteMemory import ReadWriteMemory
+import psutil
 
 if __name__ == '__main__':
-    process = ReadWriteMemory().get_process_by_name('notepad.exe')
-    process.open()
-    print(process.get_pointer(0x61AAAB8))
+    process_name = 'TMSrv.exe'
+    pid = 0x0
+
+    for proc in psutil.process_iter():
+        try:
+            if process_name in proc.name():
+                pid = proc.pid
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+            exit()
+
+    CUser_Addr = 0x61AAAB8
+    CMob_Addr = 0x7D84AC0
+
+    print(pid, CUser_Addr, CMob_Addr)
+
     pass
