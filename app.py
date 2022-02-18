920b6e1adbe4aaa6e164dcdbd9d187edc25a1573
Gabriel <gabrielturing@gmail.com>
2022-02-17 21:32:39 -0300

    validate if packet is repeated

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pymem
 import requests
 from io import BytesIO
-from ctypes import c_short, sizeof
+from ctypes import c_short, sizeof, c_ushort
 from structs import P333, P334, P338, P364, P366, P666, CUser, CMob, PacketHeader
 from tabulate import tabulate
 from base import Keys
@@ -12,12 +12,13 @@ CUSER_ADDRESS = 0x061AAAB8
 CMOB_ADDRESS = 0x07D84AC0
 PROCESS_NAME = 'TMSRVIN.exe'
 cUser = (CUser * 1000)()
-cMob = (CMob * 1000)()
+cMob = (CMob * 12800)()
 pm = pymem.Pymem("TMSRVIN.exe")
 IFACE = 'Intel(R) PRO/1000 MT Network Connection'
 FILTER = 'tcp and port 8281 and host 135.148.49.138'
 SERVER_IP = '135.148.49.138'
 API = 'https://5d1b-2804-431-c7fd-a066-993a-4c23-86e6-70b4.ngrok.io'
+TOKEN_API = 'daskd435$@$fgdg812!@!@fU'
 
 def loadBuffers() -> None:
     global cUser, cMob, pm, CUSER_ADDRESS, CMOB_ADDRESS
@@ -33,28 +34,33 @@ class Packet_666(): #PACKET KILL
         self.run()
 
     def getMobNameKiller(self) -> str:
-        return cMob[self.packet.KillerID.value].Mob.Name.decode('latin1')
+        return cMob[self.packet.KillerID].Mob.Name.decode('latin1')
 
     def getMobNameKilled(self) -> str:
-        return cMob[self.packet.KilledID.value].Mob.Name.decode('latin1')
+        return cMob[self.packet.KilledID].Mob.Name.decode('latin1')
 
     def getUsernameKiller(self) -> str:
-        return cUser[self.packet.KillerID.value].AccountName.decode('latin1')
+        if self.packet.KillerID < 0 or self.packet.KillerID > 1000:
+            return ''
+        return cUser[self.packet.KillerID].AccountName.decode('latin1')
 
     def getUsernameKilled(self) -> str:
-        return cUser[self.packet.KilledID.value].AccountName.decode('latin1')
+        if self.packet.KilledID < 0 or self.packet.KilledID > 1000:
+            return ''
+        return cUser[self.packet.KilledID].AccountName.decode('latin1')
 
     def getPosition(self) -> dict[int]:
-        return { 'x': self.packet.Pos.X.value, 'y': self.packet.Pos.Y.value }
+        return [self.packet.Pos.X, self.packet.Pos.Y]
 
     def run(self) -> None:
+        print(f'{self.getMobNameKiller()} killed {self.getMobNameKilled()} at {self.getPosition()}')
         requests.post(f'{API}/api/v1/kill', data={
             'killer': self.getUsernameKiller(),
             'killed': self.getUsernameKilled(),
             'killer_mob': self.getMobNameKiller(),
             'killed_mob': self.getMobNameKilled(),
-            'x': self.getPosition().x,
-            'y': self.getPosition().y,
+            'x': self.getPosition()[0],
+            'y': self.getPosition()[1],
         })
 
 class Packet_333(): #COMMON CHAT MESSAGE
@@ -67,10 +73,10 @@ class Packet_333(): #COMMON CHAT MESSAGE
         return self.packet.String.decode('latin1')
 
     def getUsername(self) -> str:
-        return cUser[self.packet.Heeader.ClientId.value].AccountName.decode('latin1')
+        return cUser[self.packet.Header.ClientId].AccountName.decode('latin1')
     
     def getMobName(self) -> str:
-        return cMob[self.packet.Header.ClientId.value].Mob.Name.decode('latin1')
+        return cMob[self.packet.Header.ClientId].Mob.Name.decode('latin1')
 
     def run(self) -> None:
         print(f'[{hex(self.packet.Header.PacketId)}][{self.getUsername()}] {self.getMobName()}: {self.getMessage()}')
@@ -98,6 +104,7 @@ class Packet_334(): #GLOBAL CHAT MESSAGE OR MESSAGE WITH COMMAND
         return cMob[self.packet.Header.ClientId].Mob.Name.decode('latin1')
 
     def run(self) -> None:
+        print(f'[{hex(self.packet.Header.PacketId)}][{self.getUsername()}] {self.getMobName()}: {self.getMessage()}')
         requests.post(f'{API}/api/v1/chat-messages', data={
             'username': self.getUsername(),
             'nick': self.getMobName(),
@@ -107,21 +114,17 @@ class Packet_334(): #GLOBAL CHAT MESSAGE OR MESSAGE WITH COMMAND
         })
         return
 
-
 class PacketManager(object): #PACKET MANAGER
     def __init__(self, data: bytes) -> None:
         self.buffers = []
         self.buffer = data
-        self.splitBuffer()
-        self.bufferIterator()
         self.instancePacket = {
             0x334: Packet_334,
             0x333: Packet_333,
-            #0x338: Packet_338,
-            #0x364: Packet_364,
-            #0x366: Packet_366,
             0x666: Packet_666,
         }
+        self.splitBuffer()
+        self.bufferIterator()
 
     def decrypt(self, data: bytes) -> bytes:
         data = bytearray(data)
@@ -169,21 +172,57 @@ class PacketManager(object): #PACKET MANAGER
         packet_size = c_short() 
         packet_size.value = 0
         initial_bytes = 0
+        size_increment = 0
+
         while initial_bytes < len(self.buffer):
-            io.BytesIO(self.buffer[initial_bytes:initial_bytes + 2]).readinto(packet_size)
+            if size_increment > len(self.buffer) or size_increment < 0:
+                break
+
+            if initial_bytes < 0 or initial_bytes > len(self.buffer):
+                break
+
+            io.BytesIO(self.buffer[initial_bytes:]).readinto(packet_size)
+            size_increment += packet_size.value
             n_buffer = self.buffer[initial_bytes:packet_size.value + initial_bytes]
             self.buffers.append(n_buffer)
             initial_bytes = initial_bytes + packet_size.value
 
+            if packet_size.value == 0:
+                break
+   
+    def isRepeatedPacket(self, data: bytes) -> bool:
+        response = requests.post(f'{API}/api/v1/packet-check', headers={
+            'authorization': TOKEN_API,
+        }, data={
+            'packet': data.hex(),
+        })
+
+        if response.status_code == 200:
+            return True
+        
+        return False
+
+    def addPacket(self, data: bytes) -> None:
+        requests.post(f'{API}/api/v1/add-packet', headers={
+            'authorization': TOKEN_API,
+        }, data={
+            'packet': data.hex()
+        })
+    
     def bufferIterator(self) -> None:
         for buffer in self.buffers:
+            if len(buffer) < 12:
+                continue
+
+            buffer = self.decrypt(buffer)
             packet_header = PacketHeader()
             io.BytesIO(buffer).readinto(packet_header)
             packet_id = packet_header.PacketId
             client_id = packet_header.ClientId
-            if client_id not in range(0, 1000):
-                continue
-            if packet_id in self.instancePacket.keys():
+            if packet_header.PacketId in self.instancePacket.keys():
+                if self.isRepeatedPacket(buffer):
+                    continue
+                self.addPacket(buffer)
                 self.instancePacket[packet_id](buffer)
 
 class PacketHandle(PacketManager): 
@@ -191,8 +230,8 @@ class PacketHandle(PacketManager):
         self.packetManager = super().__init__
 
     def validateBuffer(self, buffer: bytes) -> bool:
-        if buffer[IP].src == SERVER_IP:
-            return False
+        #if buffer[IP].src == SERVER_IP:
+        #    return False
         if not buffer.haslayer(Raw):
             return False
         return True
@@ -201,7 +240,8 @@ class PacketHandle(PacketManager):
         if not self.validateBuffer(buffer):
             return
 
-        self.packetManager(buffer[Raw].load)
+        loadBuffers()
+        super().__init__(buffer[Raw].load)
 
 class Application(PacketHandle): #APPLICATION SNIFFER
     def __init__(self) -> None:
@@ -225,6 +265,9 @@ class Application(PacketHandle): #APPLICATION SNIFFER
         print(tabulate(self.table, ['ID', 'CONTA', 'PERSONAGEM', 'LEVEL', 'KINGDOM', 'CLASS', 'GUILD']))
 
     def initialize(self) -> None:
+        loadBuffers()
+        self.loadTable()
+        self.dumpTable()
         global IFACE, FILTER, SERVER_IP
         sniff(iface=IFACE, filter=FILTER, store=0, prn=self.handle)
 
